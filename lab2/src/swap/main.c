f94d94662091b4e4ae971ac2a95fd15281b17b2a
nickromanchuk <roman4uk.nick@gmail.com>
2020-04-09 18:31:09 +0000

    добавлены файлы revert_string и swap

--- lab2/src/swap/main.c
+++ lab2/src/swap/main.c
@@ -1,14 +1,15 @@
+
 #include <stdio.h>
 
 #include "swap.h"
 
 int main()
 {
-	char ch1 = 'a';
-	char ch2 = 'b';
+  char ch1 = 'a';
+  char ch2 = 'b';
 
-	Swap(&ch1, &ch2);
+  Swap(&ch1, &ch2);
 
-	printf("%c %c\n", ch1, ch2);
-	return 0;
+  printf("%c %c\n", ch1, ch2);
+  return 0;
 }
